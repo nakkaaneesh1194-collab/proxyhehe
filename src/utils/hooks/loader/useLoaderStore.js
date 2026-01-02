04d8daf252c3c66c231e605c0cada75f04a29cb8
unknown <91960533+stellarcase@users.noreply.github.com>
2026-01-02 12:22:28 -0500

    [fix] menu state conflict

--- src/utils/hooks/loader/useLoaderStore.js
+++ src/utils/hooks/loader/useLoaderStore.js
@@ -18,7 +18,7 @@ const store = create((set) => ({
   iframeUrls: {},
   activeFrameRef: null,
   showUI: true,
-  toggleUI: () => set((state) => ({ showUI: !state.showUI, showMenu: false })),
+  toggleUI: () => set((state) => ({ showUI: !state.showUI })),
   setShowTabs: (value) => set({ showTabs: value }),
   toggleTabs: () => set((state) => ({ showTabs: !state.showTabs })),
   toggleMenu: () => set((state) => ({ showMenu: !state.showMenu })),
